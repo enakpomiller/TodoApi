9abe4b35a643155c7fac8ae37da781e641785c05
enakpomiller <63649279+enakpomiller@users.noreply.github.com>
2024-01-01 18:40:39 +0100

    adding records to list

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,35 +1,38 @@
-import {Model } from 'sequelize';
-import db from '../config/database.config';
+ //import {Model } from 'sequelize';
+ import {DataTypes, Model} from 'sequelize';
+ import db from '../config/database.config';
 
-   import {DataTypes} from 'sequelize';
+  
 
 
-interface TodoAttribute {
-    id:String,
-    title:String,
+interface TodoAttributes {
+    id:string,
+    title:string,
     completed: boolean;
 
 }
 
 
-export class TodoInstance extends Model<TodoAttribute> {}
+export class TodoInstance extends Model<TodoAttributes> {}
 
-TodoInstance.init(
+TodoInstance.init(  
         {
-          id:{
-             type: DataTypes.STRING,
+          id: {
+             type: DataTypes.UUID, 
              primaryKey:true,
              allowNull:false,
            },
+
            title:{
              type: DataTypes.STRING,
              allowNull:false,
            },
+
            completed:{
                 type: DataTypes.BOOLEAN,
                 allowNull:false,
                 defaultValue:false,
-            }
+            },
 
 
         },
--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,50 @@
 
 import express, { Request, Response } from  'express';
 import db from './config/database.config';
+import { v4 as uuidv4 } from 'uuid';
+import { TodoInstance } from './model/index';
 
  
-// test database connection
-db.sync().then(() =>{
-  console.log('connected to database was  successfully');
-});
+
 
 
 
 const app = express();
+app.use(express.json()); // body parser
+
+ 
+
+        app.post('/create',async(req:Request,res:Response) => {
+           const id = uuidv4();
+           try{
+            const record = await TodoInstance.create({ ...req.body, id}); 
+            return res.json({record,msg:' succssfully created'});
+          }catch(error){
+            return res.json({msg:' cannot create ', status:201,route: '/create'});
+          }
+
+        }); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test database connection
+db.sync().then(() =>{
+    console.log('connected to database was  successfully');
+  });
 
 
 
 const port = 5300;
-app.get('/',(req: Request, res: Response) => {
-  return res.send('hello'); 
-});
-
-
-
-
 
 
 app.listen(port, () => {
